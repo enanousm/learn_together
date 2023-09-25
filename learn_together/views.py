de862aca5e6122fe78b4de9069a501d8df5ddc2b
enanousm <cvillalobos@usm.cl>
2023-09-25 16:46:37 -0300

    aplicar html

--- learn_together/views.py
+++ learn_together/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.template import Template,Context
+from django.template import Template, Context
 
 def hola(request):
-    plantillaExt = open('C:\Users\enano\Documents\Enano\Int a la Ingenieria 2023\learn_together\learn_together\templates\plantilla_hola.html')
+    plantillaExt = open("C:\Users\enano\Documents\Enano\Int a la Ingenieria 2023\learn_together\learn_together\templates\plantilla_hola.html")
     template = Template(plantillaExt.read())
     plantillaExt.close()
     contexto = Context()
