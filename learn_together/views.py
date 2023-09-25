55bb69e31cca3a4d9ab3dab0945cf3043a1df047
enanousm <cvillalobos@usm.cl>
2023-09-25 16:09:11 -0300

    probando plantilla

---
+++ learn_together/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import Template,Context
+
+def hola(request):
+    plantillaExt = open('C:\Users\enano\Documents\Enano\Int a la Ingenieria 2023\learn_together\learn_together\templates\plantilla_hola.html')
+    template = Template(plantillaExt.read())
+    plantillaExt.close()
+    contexto = Context()
+    documento = template.render(contexto)
+    return HttpResponse(documento)
