2ab8e4f3ffd32ba1755f61d2c6224ca4ae21c79f
enanousm <cvillalobos@usm.cl>
2023-10-11 18:46:11 -0300

    a

--- learn_together/database/controller.py
+++ learn_together/database/controller.py
@@ -1,6 +1,7 @@
 import sqlite3 as sql
 url = 'learn_together/database/database.db'
 
+#crear la tabla si esta no existe#
 c = sql.connect(url)
 cursor = c.cursor()
 cursor.execute('''
@@ -17,6 +18,7 @@ cursor.execute('''
 c.commit()
 c.close()
 
+#insertar nuevo usuario en la base de datos#
 def insert_row(nombre, apellido, sexo, correo, contraseña, rol, horario):
     instruction = f'''INSERT INTO userdata VALUES('{nombre}','{apellido}','{sexo}','{correo}','{contraseña}','{rol}','{horario}')'''
     c = sql.connect(url)    
@@ -25,24 +27,27 @@ def insert_row(nombre, apellido, sexo, correo, contraseña, rol, horario):
     c.commit()
     c.close()
 
+#actualizar horario del usuario#
 def update_horario(correo,nuevo_horario):
-    instruction = f'''UPDATE userdata SET horarios='{nuevo_horario}' WHERE correo IS '{correo}' '''
+    instruction = f'''UPDATE userdata SET horarios='{nuevo_horario}' WHERE correo='{correo}' '''
     c = sql.connect(url)    
     cursor = c.cursor()
     cursor.execute(instruction)
     c.commit()
     c.close()
 
+#borrar usuario de la base de datos#
 def delete_user(correo):
-    instruction = f'''DELETE FROM userdata WHERE correo IS '{correo}' '''
+    instruction = f'''DELETE FROM userdata WHERE correo='{correo}' '''
     c = sql.connect(url)    
     cursor = c.cursor()
     cursor.execute(instruction)
     c.commit()
     c.close()
 
+#buscar y retornar todos los campos del usuario#
 def search_user(correo):
-    instruction = f'''SELECT * FROM userdata WHERE correo IS '{correo}' '''
+    instruction = f'''SELECT * FROM userdata WHERE correo='{correo}' '''
     c = sql.connect(url)    
     cursor = c.cursor()
     cursor.execute(instruction)
@@ -50,9 +55,11 @@ def search_user(correo):
     c.commit()
     c.close()
     return data[0]
+    #retorna una tupla
 
+#buscar y retornar SOLO EL HORARIO del usuario#
 def search_horario(correo):
-    instruction = f'''SELECT horarios FROM userdata WHERE correo IS '{correo}' '''
+    instruction = f'''SELECT horarios FROM userdata WHERE correo='{correo}' '''
     c = sql.connect(url)    
     cursor = c.cursor()
     cursor.execute(instruction)
@@ -60,6 +67,7 @@ def search_horario(correo):
     c.commit()
     c.close()
     return data[0][0]
+    #retorna un string
 
 print(search_user('jureta'))
 print(search_horario('jureta'))
