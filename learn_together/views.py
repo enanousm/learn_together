37f13532ca1f1b3d96ec2bb964bed4e5f2f3fa19
enanousm <cvillalobos@usm.cl>
2023-10-22 14:01:58 -0300

    a

--- learn_together/views.py
+++ learn_together/views.py
@@ -1,18 +1,12 @@
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import HttpResponse
 from django.shortcuts import render
 from . import controller as cont
 
+
 def hola(request):
     return render(request, 'registro.html')
 
-def registrar(r):
-    nombre = r.POST['nombre']
-    apellido = r.POST['apellido']
-    sexo = r.POST['sexo']
-    correo = r.POST['correo']
-    contraseña = r.POST['contraseña']
-    rol = r.POST['rol']
-    horario = r.POST['horario']
+def registrar(request):
     
-    mensaje = 'Estimado {}, sus datos han sido registrados satisfactoriamente'.format(nombre)
-    return HttpResponse('Datos registrados')
+    return HttpResponse('hola')
