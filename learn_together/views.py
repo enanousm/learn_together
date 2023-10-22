93f5f1d2094ee87e6c8a0c28397dc242f39b7689
enanousm <cvillalobos@usm.cl>
2023-10-22 15:39:32 -0300

    a

--- learn_together/views.py
+++ learn_together/views.py
@@ -3,10 +3,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from . import controller as cont
 
-
 def hola(request):
     return render(request, 'registro.html')
 
 def registrar(request):
-    
-    return HttpResponse('hola')
+    nombre = request.POST['nombre']
+    apellido = request.POST['apellido']
+    contraseña = request.POST['contraseña']
+    correo = request.POST['correo']
+    rol = request.POST['rol']
+    horarios = request.POST['horarios']
+    if 'sexo' in request.POST:
+        sexo = request.POST['sexo']
+    else:
+        sexo = None
+    horario = None
+    return HttpResponse()
